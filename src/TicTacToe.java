15923fa7ef53537e13a9f2e4cb8ca60932c092c2
Mathis <math6s667@gmail.com>
2026-04-13 00:52:20 -0200

    Creation de la classe TicTacToe et le constructeur qui affiche la fenetre

---
+++ src/TicTacToe.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class TicTacToe {
+    
+    int boardWidth = 600;
+    int boardHeight = 650;
+
+    JFrame frame = new JFrame();
+    
+        TicTacToe(){
+            frame.setVisible(true);
+            frame.setSize(boardWidth, boardHeight);
+            frame.setLocationRelativeTo(null);
+            frame.setResizable(false);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setLayout(new BorderLayout());
+        }
+}
