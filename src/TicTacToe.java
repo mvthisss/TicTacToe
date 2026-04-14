8ae165d3cb7169a33bd093f75e92eee1845dad42
Mathis <math6s667@gmail.com>
2026-04-13 22:26:55 -0200

    Ajout de la gride qui separe la fenetre en 3x3 carreau, notion de JFrame,JLabel, JButton

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -7,8 +7,16 @@ public class TicTacToe {
     int boardWidth = 600;
     int boardHeight = 650;
 
-    JFrame frame = new JFrame();
-    
+    JFrame frame = new JFrame("Tic-Tac-Toe");
+    JLabel textLabel = new JLabel();
+    JPanel textPanel= new JPanel();
+    JPanel boardPanel = new JPanel();
+
+    JButton[][] board = new JButton [3][3];
+    String player1 = "X";
+    String player2 = "O";
+    String currentPlayer = player1;
+
         TicTacToe(){
             frame.setVisible(true);
             frame.setSize(boardWidth, boardHeight);
@@ -16,5 +24,38 @@ public class TicTacToe {
             frame.setResizable(false);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setLayout(new BorderLayout());
-        }
-}
+
+            textLabel.setBackground(Color.darkGray);
+            textLabel.setForeground(Color.white);
+            textLabel.setFont(new Font("Arial", Font.BOLD, 50));
+            textLabel.setHorizontalAlignment(JLabel.CENTER);
+            textLabel.setText("Tic Tac Toe");
+            textLabel.setOpaque(true);
+
+           textPanel.setLayout(new BorderLayout());
+            textPanel.add(textLabel);
+            frame.add(textPanel,BorderLayout.NORTH);
+
+            boardPanel.setLayout(new GridLayout(3,3));
+            boardPanel.setBackground(Color.LIGHT_GRAY);
+            
+
+            frame.add(boardPanel);
+
+            for(int r=0; r<3; r++){
+                for(int c=0; c<3; c++){
+                    JButton tile = new JButton();
+                    board[r][c] = tile;
+                    board[r][c].setFont(new Font("Arial", Font.BOLD, 100));
+                    boardPanel.add(board[r][c]);
+
+                    tile.setBackground(Color.black);
+                    tile.setForeground(Color.white);
+                    tile.setFocusable(false);
+                  
+                            }
+                        }
+                    }
+                }
+            
+    
